b3285cf59bdff57e582228f0fac20d435a34b784
Billy <billaqn1202@gmail.con>
2024-12-11 01:34:08 +0700

    Added route and change app.js

---
+++ api/routes/order.routes.js
@@ -0,0 +1,12 @@
+module.exports = (app) => {
+    const orders = require("../controllers/order.controller.js");
+    var router = require("express").Router();
+  
+    router.post("/orders/", orders.create);
+    router.get("/orders/customer/:customerId", orders.findAll);
+    router.put("/orders/:orderId", orders.update);
+    router.delete("/orders/:orderId", orders.delete);
+  
+    app.use("/api", router);
+  };
+  
